a13f92ac78eb13c1c77c58e47ffe043f6ab50807
ivsnksss <153433446+ivsnksss@users.noreply.github.com>
2024-12-01 16:42:32 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,124 @@
+
+#print("Hello World!")
+
+#Тип змінної int, цілі числа
+#number = 10
+#Ти змінної float, не цілі числа
+#price = 9.99
+#Тип змінної string, рядок або текст
+#name = "Valentine"
+#Тип змінної bool(bolean), True або False(1 або 0)
+#light = True
+
+#book = 240
+#rubber = 15
+#print(book)
+#print(rubber)
+#print(type(book))
+
+#pencil = 12.50
+#pen = 15.80
+#print(pencil)
+#print(pen)
+#print(type(pen))
+
+#boy = "Artem"
+#girl = "Veronika"
+#print(boy)
+#print(girl)
+#print(type(boy))
+
+#Electricity = True
+#Water = False
+#print(Electricity)
+#print(Water)
+#print(type(Water))
+
+# tymophi = 8
+# matvii = 7
+
+# if tymophi > 6:
+#     print("12 points tymophi")
+# elif matvii > 6:
+#     print("12 points matvii")
+# else:
+#     print ("boys have 2 points")
+
+
+
+# Artem = 40
+# Veronika = 51
+
+# if Artem > 35:
+#     print("Great job, you colleced 35 apples!THE TASK IS COMPLETED! ARTEM!")
+# elif Veronika > 35:
+#     print("Great job, you colleced 35 apples!THE TASK IS COMPLETED! VERONIKA!")
+# else:
+#     ("You did not comleted your task")
+
+
+
+# for i in range(10):
+#     print("Hello World!")
+
+
+# a = 5
+# while a < 10:
+#     a += 1
+#     print(a)
+
+
+# for i  in range(12):
+#     print("Go to bed")
+
+# time = 3
+# while time < 6:
+#     time += 1
+#     print(time)
+
+
+# Artem = 12
+# Veronika = 13
+
+# if Veronika > 10:
+#     for i in range(4):
+#         print("Veronika is oldest than 10 years")
+# elif Artem > 15:
+#     while Artem > 15:
+#         Artem += 3
+#         print(Artem)
+# else:
+#     print(Artem, Veronika)
+
+# cherry = 2
+
+# if cherry < 3:
+#     for i in range(3):
+#         print("Oh, no!")
+# elif cherry < 5:
+#     for i in range(3):
+#         print("Yes!")
+# else:
+#     while cherry < 6:
+#         cherry += 1
+#         print(cherry)
+
+
+# num = 5
+
+# def game():
+#     num = 10
+#     print(num)
+
+# print(num)
+# game()
+
+
+
+
+
+
+
+
+
+
