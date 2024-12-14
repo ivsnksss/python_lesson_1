f6a8e266158fc200bef4aea05d4f3d06d1cc4c99
ivsnksss <153433446+ivsnksss@users.noreply.github.com>
2024-12-14 21:59:45 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+name = "Ivanka"
+age = 11
+height = 158.9
+student = True
+
+if age > 6:
+    print("Іванка старша 6 років!")
+elif height < 170:
+    print("Іванка ти менша за 170 см")
+elif name == "Ivanka":
+    print("Тебе звати Іванка!")
+else:
+    print("Я тебе не розумію")
